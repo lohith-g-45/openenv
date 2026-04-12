c82dbad5ad56b52063d9619148f6a2eb9629bdaf
Lohith G <lohithg_aiml@ksit.edu.in>
2026-04-12 21:24:35 +0530

    Finalize cleanup and grader package import consistency

--- your/grader/__init__.py
+++ your/grader/__init__.py
@@ -1,4 +1,4 @@
 """Compatibility grader package."""
-from .module import EasyGrader, MediumGrader, HardGrader
+from grader import EasyGrader, MediumGrader, HardGrader
 
 __all__ = ["EasyGrader", "MediumGrader", "HardGrader"]
